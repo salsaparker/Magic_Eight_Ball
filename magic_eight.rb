4f2dd9981283e125ed729811654b5a091d0444ce
Tyler Parker <tparker42d2@gmail.com>
2015-05-28 15:04:36 -0600

    refactored @answer_arr, added display_compresponse method

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,42 +1,36 @@
 class MagicEight
     def greeting_instructions
-        print "~~ Welcome to the magic eight ball ~~"
+        puts "~~ Welcome to the magic eight ball ~~"
         sleep 0.25
-        print "Ask me anything, hombre."
+        puts "Ask me anything, hombre."
         @user_response = gets.chomp
     end #greeting end
 
     def comp_response 
-      @comp_answer = (0..9).to_a.sample
-       puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-        while @comp_answer == 1
-          puts "Perhaps"
-        end
-        while @comp_answer == 2
-          puts "Fat chance, hombre"
-        end
-        while @comp_answer == 3
-          puts "Purple! Because aliens don't wear hats!" 
-        end
-        while @comp_answer == 4
-          puts "Salsa deems it unworthy. Pesant." 
-        end
-        while @comp_answer == 5
-          puts "Go ask, the Griz. He seems like a wise dog."
-        end
-        while @comp_answer == 6
-          puts "It's on like Donkey Kong!"
-        end
-        while @comp_answer == 7
-          puts "Dunno. Google it."
-        end
-        while @comp_answer == 8
-          puts "What's the point?"
-        end 
-        while @comp_answer == 9
-          "Thou shall count to 3 with the holy hand granade, not 2! Because 2 is too short
-          and 4 being too long... and 5 is right out."
-        end 
-    end #comp_response class end
+      @answer_arr = ["Perhaps", 
+                    "Fat chance, hombre", 
+                    "Purple! Because aliens don't wear hats!",
+                    "Salsa deems it unworthy. Pesant.",
+                    "Go ask, the Griz. He seems like a wise dog.",
+                    "It's on like Donkey Kong!",
+                    "Dunno. Google it.",
+                    "What's the point?",
+                    "Thou shall count to 3 with the holy hand granade, not 2! 
+                    \nBecause 2 is too short and 4 being too long... and 5 is right out."
+                    ]
+    end
+    def display_compresponse
+      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+      @answer_arr.sample
+    end
+
+    def play
+      greeting_instructions
+      comp_response
+    end
 
 end # MagicEight end
+
+m = MagicEight.new
+m.play
+
