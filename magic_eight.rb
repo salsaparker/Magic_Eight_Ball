45410b4bdda063648a5844a52469146684734a8a
Tyler Parker <tparker42d2@gmail.com>
2015-05-28 12:04:11 -0600

    made user response greeting

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,5 +1,10 @@
 class MagicEight
+    def greeting_instructions
+        print "~~ Welcome to the magic eight ball ~~"
+        sleep 0.25
+        print "Ask me anything, hombre."
+        @user_response = gets.chomp
+    end
 
-  'stuff n things'
 
 end
