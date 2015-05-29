946d81f4af0c6049418bdf3501df36fc2429d7bb
Tyler Parker <tparker42d2@gmail.com>
2015-05-28 22:28:05 -0600

    refactored and added comments

--- magic_eight.rb
+++ magic_eight.rb
@@ -1,16 +1,16 @@
 class MagicEight
-    def greeting_instructions
-        puts "~~ Welcome to the magic eight ball ~~"
-        sleep 0.25
-        puts "Ask me anything, hombre."
-        @user_response = gets.chomp.to_s
-        if @user_response == 'quit'
+    def greeting_instructions #define method 'greeting_instruction'
+        puts "~~ Welcome to the magic eight ball ~~" 
+        sleep 0.25  #gives a little bit of buffer time between the welcoming and instructions
+        puts "Ask me anything, hombre." #instructions
+        @user_response = gets.chomp.to_s.downcase #assigns user input to a variable '@user_response'
+        if @user_response == 'quit'   #if statement that has a conditional to exit the game if user input is 'quit'
             puts 'Fine, be a quitter.'
           exit()
         end
-    end #greeting end
+    end #greeting method end
 
-    def comp_response 
+    def comp_response   #defines the method that holds our array of responses the computer executes after user responds
       answer_arr = ["Perhaps", 
                     "Fat chance, hombre", 
                     "Purple! Because aliens don't wear hats!",
@@ -19,37 +19,36 @@ class MagicEight
                     "It's on like Donkey Kong!",
                     "Dunno. Google it.",
                     "What's the point?",
-                    "Thou shall count to 3 with the holy hand granade, not 2! 
-                    \nWhile 2 is too short and 4 being too long... and 5 is right out."
+                    "This next test may involve trace amounts of time travel. \nSo, word of advice: \nIf you meet yourself on the testing track, don't make eye contact. \nLab boys tell me that'll wipe out time. \nEntirely. \nForward and backward! \nSo do both of yourselves a favor and just let that handsome devil go about his business.\n",
+                    "Those of you helping us test the Repulsion Gel today, just follow the blue line on the floor. \nThose of you who volunteered to be injected with praying mantis DNA, I've got some good news and some bad news. \nBad news is we're postponing those tests indefinitely. \nGood news is we've got a much better test for you: fighting an army of mantis men. \nPick up a rifle and follow the yellow line. \nYou'll know when the test starts.",
+                    "Great work! \nBecause this message is prerecorded, any observations related to your performance are speculation on our part. \nPlease disregard any undeserved compliments.",
+                    "First thou pullest the Holy Pin. \nThen thou must count to three. \nThree shall be the number of the counting and the number of the counting shall be three. \nFour shalt thou not count, neither shalt thou count two, \nexcepting that thou then proceedeth to three. \nFive is right out.",
                     ]
-      display_comp_response(answer_arr)
+      display_comp_response(answer_arr) #executes method below, passes the array above as and arguement for the 'display_comp_response'
     end
     def display_comp_response(answer_arr)
       puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-      puts answer_arr.sample
+      sleep 0.5   #buffers
+      puts answer_arr.sample  #calls on a random response in the array
+      sleep 0.20  #buffers
     end
 
-    def play_again
+    def play_again  #method that gives the user the option to play again, user can say anything and the game will execute again
+      sleep 1
       puts "Anything else?"
       puts "Type 'quit' to be a quitter."
-      @user_response = gets.chomp.to_s
+      @user_response = gets.chomp.to_s.downcase
       if @user_response == 'quit'
         puts 'Fine, be a quitter.'
-        exit()
-      # else
-      #   play
+        exit()  #if conditional
       end
     end
 
     def play
-    # begin
       greeting_instructions
       while true
       comp_response
-      # display_comp_response
       play_again
-    # until @user_response == 'quit'
-    # break
       end #while end statement
     end #play method end
 end # MagicEight end
